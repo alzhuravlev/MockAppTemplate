02881e98af6c802076fdbc6e87a8052e0873631d
Alexey Zhuravlev <azhuravlev@acumatica.com>
2019-11-11 20:53:00 +0300

    refactor

--- app/src/main/java/com/crane/demo/mockapptemplate/LaunchActivity.kt
+++ app/src/main/java/com/crane/demo/mockapptemplate/LaunchActivity.kt
@@ -2,28 +2,27 @@ package com.crane.demo.mockapptemplate
 
 import com.crane.mockapp.core.MockApp
 import com.crane.mockapp.core.MockAppActivity
+import com.crane.mockapp.core.MockAppService
 import com.crane.mockapp.core.model.layouts.LayoutDescriptor
 import com.crane.mockapp.core.model.layouts.ProjectModel
-import com.crane.mockapp.core.model.layouts.ProjectService
-import com.crane.mockapp.core.model.layouts.ProjectServiceFactory
 
 class LaunchActivity : MockAppActivity() {
 
     override fun resolveLayoutDescriptor(): LayoutDescriptor? {
 
-        val projects = ProjectServiceFactory.getInstance().findProjectModels { pm ->
+        val projects = MockApp.getInstance().findProjectModels { pm ->
             pm.projectType == ProjectModel.ProjectType.INTERNAL
         }
         if (projects.isEmpty())
             return null
         val project = projects.get(0)
 
-        val mainLayoutId = ProjectServiceFactory.getInstance().getMainLayoutId(project.id)
-        var layout = ProjectServiceFactory.getInstance().findLayoutModel(project.id, mainLayoutId)
+        val mainLayoutId = MockApp.getInstance().getMainLayoutId(project.id)
+        var layout = MockApp.getInstance().findLayoutModel(project.id, mainLayoutId)
         if (layout == null) {
             val layouts =
-                ProjectServiceFactory.getInstance()
-                    .findLayoutModels(ProjectService.LAYOUT_FILTER_ALL, project.id)
+                MockApp.getInstance()
+                    .findLayoutModels(MockAppService.LAYOUT_FILTER_ALL, project.id)
             if (layouts.isEmpty())
                 return null
             layout = layouts.get(0)
